63710aebcfd1503ba008bd05bc2bcb98905e8aa4
Pablo Jorge <pablo.jorge@xapo.com>
2019-08-11 22:53:18 -0300

    Fix OKCoin plugin

--- app/plugins/OKCoin.js
+++ app/plugins/OKCoin.js
@@ -15,29 +15,43 @@ function OKCoinPriceRequester(symbol, options) {
 OKCoinPriceRequester.config = {
     exchange: 'okcoin',
     symbol_map: {
-        "BTCUSD" : "btc_usd",
-        "ETHUSD" : "eth_usd",
-        "LTCUSD" : "ltc_usd",
+        "BTCUSD" : undefined,
+        "ETHUSD" : undefined,
+        "LTCUSD" : undefined,
     },
-    url_template: 'https://www.okcoin.com/api/ticker.do?symbol=<<SYMBOL>>&ok=1',
+    url_template: 'https://www.okcoin.com/v2/spot/markets/tickers',
 };
 
 OKCoinPriceRequester.prototype = Object.create(PriceRequester.prototype);
 OKCoinPriceRequester.prototype.constructor = OKCoinPriceRequester;
 
 OKCoinPriceRequester.prototype.processResponse = function (response, body) {
-    var ticker = JSON.parse(body).ticker,
-        bid = parseFloat(ticker.buy),
-        ask = parseFloat(ticker.sell);
-    return new messages.Symbol(this.getExchange(),
-                               this.symbol,
-                               bid,
-                               ask,
-                               new Date(), {
-                                   volume24: parseFloat(ticker.vol),
-                                   high24: parseFloat(ticker.high),
-                                   low24: parseFloat(ticker.low),
-                               });
+    var symbol_map = {
+        "BTCUSD" : "btc_usd",
+        "ETHUSD" : "eth_usd",
+        "LTCUSD" : "ltc_usd",
+    };
+    var self = this;
+    var tickers = JSON.parse(body).data,
+        symbol = null;
+
+    tickers.forEach(function(ticker) {
+        if (ticker.symbol == symbol_map[self.symbol]) {
+            symbol = new messages.Symbol(
+                self.getExchange(),
+                self.symbol,
+                parseFloat(ticker.buy),
+                parseFloat(ticker.sell),
+                new Date(), {
+                    volume24: parseFloat(ticker.vol),
+                    high24: parseFloat(ticker.high),
+                    low24: parseFloat(ticker.low),
+                }
+            );
+        }
+    });
+
+    return symbol;
 };
 /**/
 
